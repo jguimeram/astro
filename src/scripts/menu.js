4b30fc2ecd2632ea6e1118b26eb5ba744b9e78c1
jguimeram <jguimera54@gmail.com>
2025-02-16 15:53:26 +0100

    slots

---
+++ src/scripts/menu.js
@@ -0,0 +1,5 @@
+/* This script is for footer component */
+
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('expanded');
+});
